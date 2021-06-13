92dfaf83e28a273ff271dffbd7c211ed019df5b4
boyloe <boyloe@gmail.com>
2021-06-13 18:13:23 -0400

    Add linting, and prettier

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from "react";
+
+export default function index(): ReactElement {
+  return (
+    <div>
+      <h1>This will be the home page for WanderLump</h1>
+    </div>
+  );
+}

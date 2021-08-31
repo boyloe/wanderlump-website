75639899473df4a0adf1339cf7af9760107844c8
boyloe <boyloe@gmail.com>
2021-08-30 23:30:25 -0400

    Add first blog post

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withImages = require("next-images");
 (module.exports = withImages()), { target: "serverless" };
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withMDX = require("@next/mdx")();
-module.exports = withMDX();

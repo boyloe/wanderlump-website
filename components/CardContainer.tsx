be7d2c1461f746c4be6803d4e4d62e6d8d9035e1
boyloe <boyloe@gmail.com>
2021-09-04 00:42:21 -0400

    update card and container props

--- components/CardContainer.tsx
+++ components/CardContainer.tsx
@@ -1,11 +1,22 @@
 import React, { ReactElement } from "react";
-import NewTrailer from "../public/images/sold_front.jpeg";
 import Card from "./Card";
 
+// Only doing 3 Cards per container
 export default function CardContainer(): ReactElement {
   return (
     <div className="pt-20 flex flex-col sm:flex-row items-center md:justify-evenly w-full">
-      <Card cardImageUrl={NewTrailer} />
+      <Card
+        cardImageUrl="/images/sold_front.jpeg"
+        cardTitle="Our New Home"
+        href="/posts/our-new-home"
+        date="April 20, 2021"
+      />
+      <Card
+        cardImageUrl="/images/posts/griffs-campground/griffs-sign.JPG"
+        cardTitle="Griff's RV Park"
+        href="/posts/griffs-campground"
+        date="April 24, 2021"
+      />
     </div>
   );
 }

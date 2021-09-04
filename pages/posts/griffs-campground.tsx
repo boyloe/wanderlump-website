c317f759c43209320c507f17739cc3f2fa1263fb
boyloe <boyloe@gmail.com>
2021-09-04 00:42:40 -0400

    add griffs-campground post and images

---
+++ pages/posts/griffs-campground.tsx
@@ -0,0 +1,83 @@
+import React, { ReactElement } from "react";
+
+export default function griffsCampground(): ReactElement {
+  return (
+    <div className="mx-auto max-w-2xl p-4 md:p-0">
+      <title>Griff&apos;s RV Park and Campground</title>
+      <article>
+        <h1 className="text-5xl mb-4 font-barlow-condensed">
+          Griff&apos;s RV Park and Campground
+        </h1>
+        <img src="/images/posts/griffs-campground/griffs-sign.JPG" alt="" />
+        <p className="my-4 font-barlow-condensed text-2xl">
+          After picking up the trailer and getting an incredibly thorough walk
+          through from Brady at Herold Trailer Sales, we needed a place nearby
+          to unload everything from the truck into the trailer. We were also
+          exhausted from the previous few days of packing and driving to Iowa,
+          so we settled on a full hook up site just 30 minutes north in Altoona,
+          Iowa. I don’t think we could have found a better site for our first
+          stay and to get unpacked! Our spot was right on a pond, the sites were
+          all concrete, perfectly level pads with lush green grass all around.
+          We were lucky to even get a spot, because most of the park was full.
+        </p>
+        <h2 className="my-4 text-3xl font-barlow-condensed font-black">
+          You’re welcome to our neighbors who got a pretty good show
+        </h2>
+        <p className="my-4 font-barlow-condensed text-2xl">
+          For it being Bryan’s first time backing up our new trailer, he was a
+          rock star. Perfect back up job on the first attempt. This was not
+          luck, though. He grew up towing boats and his previous career in the
+          oil industry required him to back up trailers frequently. Lumpy was
+          pretty impressed. Catalina was too busy sleeping in her carrier to be
+          bothered.{" "}
+        </p>
+        <p className="my-4 font-barlow-condensed text-2xl">
+          What came next was entertaining for anyone peeking out their window.
+          It was probably the world’s longest set up. In our defense, all of our
+          stuff was brand new and either still in the box or in the storage
+          bins, so it took a while to open everything up. We weren’t just
+          winging it; we had prepared and downloaded the incredibly handy
+          checklist, so we went step by step and after two hours, we felt fairly
+          confident we wouldn’t roll away into the pond or blow up.{" "}
+        </p>
+        <img
+          src="/images/posts/griffs-campground/truck-trailer.JPG"
+          alt="Bryan getting the trailer disconnected"
+        />
+        <h2 className="mt-4 text-3xl font-barlow-condensed font-black">
+          Introductions
+        </h2>
+        <p className="my-4 font-barlow-condensed text-2xl">
+          Now that we were fully set up, it was time for the most crucial step –
+          introducing the cats to their new home. What if they hated it? What if
+          they were so stressed that they never came out of hiding? What if all
+          this planning was for nothing? Our fears quickly subsided as they took
+          time to sniff and explore every inch and then they both made
+          themselves at home on the bed.
+        </p>
+        <img
+          src="/images/posts/griffs-campground/lumpy-catalina.JPG"
+          alt="Lumpy and Catalina asleep on the bed"
+        />
+        <h2 className="mt-4 text-3xl font-barlow-condensed font-black">
+          All work and no play?
+        </h2>
+        <p className="my-4 font-barlow-condensed text-2xl">
+          There was so much work to do – unpacking, organizing, making endless
+          shopping lists of additional supplies, and working our full time jobs.
+          However, there was a huge weight off our shoulders. We were actually
+          doing this! Now was the time to have some fun, too. We explored
+          Griff’s and the many statues scattered throughout the park. We checked
+          out the setup of the other rigs and took some notes. We explored
+          Altoona, tried some local beer, planned our next travels, and made our
+          travel trailer feel like home. Next stop – Round Lake Winery in Round
+          Lake, Minnesota.
+        </p>
+        <img
+          src="/images/posts/griffs-campground/shelly-cowboy.JPG"
+          alt="Shelly pretending to be a cowgirl"
+        />
+      </article>
+    </div>
+  );
+}

5129ffc07d63933261cd47d6032c1bf8d891f93d
boyloe <boyloe@gmail.com>
2021-09-11 14:17:39 -0400

    create horizontal card component for blog index

---
+++ components/HorizontalCard.tsx
@@ -0,0 +1,48 @@
+import React, { ReactElement } from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+interface IHorizontalCardProps {
+  title?: string;
+  date?: string;
+  href: string;
+  backgroundSrc: string;
+  preview: string;
+  location: string;
+}
+
+export default function HorizontalCard({
+  title,
+  date,
+  href,
+  backgroundSrc,
+  preview,
+  location,
+}: IHorizontalCardProps): ReactElement {
+  return (
+    <Link href={href}>
+      <div className="flex items-center p-4 bg-white border-2 border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 font-lato">
+        <div className="hidden sm:flex">
+          <Image
+            alt={backgroundSrc}
+            src={backgroundSrc}
+            height={200}
+            width={160}
+          />
+        </div>
+        <div id="body" className="flex flex-col ml-5">
+          <h4 id="name" className="text-xl font-semibold mb-2">
+            {title}
+          </h4>
+          <p id="job" className="text-gray-800 mt-2">
+            {preview}
+          </p>
+          <div className="flex flex-col mt-5">
+            <p className="text-sm ">{date}</p>
+            <p className="text-sm italic">{location}</p>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+}

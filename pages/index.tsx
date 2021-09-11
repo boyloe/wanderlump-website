b08830b17fc10438645299b77ca620f69d704e53
boyloe <boyloe@gmail.com>
2021-09-11 14:16:44 -0400

    Update home page and hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,17 +15,32 @@ export default function Index(): ReactElement {
         <div className="flex flex-col md:flex-row justify-evenly items-center">
           <img
             src="/images/DH-Bryan-Shelly.jpg"
-            className="w-auto md:w-1/3 md:rounded-2xl"
+            className="w-auto md:w-1/3 md:rounded-2xl md:border-4 md:border-black shadow-2xl"
           />
-          <p className="w-screen md:w-1/3 md:text-2xl px-6 pt-12 font-josefin-sans">
-            Welcome to wanderlump! We are Bryan and Shelly and we are excited to
-            share our adventure with you. We decided to take our lives, our
-            jobs, and our two cats on the road and become full-time travelers.
-          </p>
+          <div className="w-screen md:w-1/3 md:text-2xl px-6 pt-12 font-josefin-sans">
+            <p>
+              Welcome to wanderlump! We are Bryan and Shelly and we are excited
+              to share our adventure with you. We decided to take our lives, our
+              jobs, and our two cats on the road and become full-time travelers.
+            </p>
+            <br />
+            <p>
+              Shelly was already working remotely and Bryan got a fully remote
+              position in February 2021. Just before Bryan got his job, our
+              apartment complex gave us renewal papers and the rent went up
+              significantly. We were already spending a small fortune on our one
+              bed/one bath apartment outside of Denver. We took a step back and
+              looked at our lives. Being apartment poor was not what we wanted
+              anymore. After many calls with family and friends to make sure we
+              weren&apos;t just acting COVID crazy, endless research and Excel
+              spreadsheets, and getting permission from both cats, we officially
+              moved out of our apartment in April and became full-time
+              travelers.
+            </p>
+          </div>
         </div>
       </div>
       <Subscribe />
     </Fragment>
-    // eslint-disable-next-line prettier/prettier
-  )
+  );
 }

b6def5581e163ed28a5b224d6a1b8597438b4b3a
boyloe <boyloe@gmail.com>
2021-09-14 12:03:01 -0400

    Change button color for prevNext

--- components/PrevNext.tsx
+++ components/PrevNext.tsx
@@ -12,12 +12,12 @@ export default function PrevNext({
   return (
     <div className="flex justify-center mb-8">
       <Link href={previousPostHref}>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-4">
+        <button className="bg-gray-600 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded mx-4">
           Prev Post
         </button>
       </Link>
       <Link href={nextPostHref}>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-4">
+        <button className="bg-gray-600 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded mx-4">
           Next Post
         </button>
       </Link>

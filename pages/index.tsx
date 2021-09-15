28a8324d1579a948b00d3767436130f18d831112
boyloe <boyloe@gmail.com>
2021-09-14 22:32:05 -0400

    update latest post

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,12 +35,12 @@ export default function Index(): ReactElement {
           <h3 className="text-xl md:text-3xl mb-4">Latest Post:</h3>
           <div className="shadow-xl">
             <HorizontalCard
-              title="Custer State Park and the Black Hills"
-              date="May 1, 2021"
-              location="Black Hills, SD"
-              href="/posts/custer-state"
-              backgroundSrc="/images/posts/custer-state/black-elk-snow-rock.JPG"
-              preview="Baby bison and wifi sheep, who could ask for more..."
+              title="Wall Drug"
+              date="May 3, 2021"
+              location="Wall, SD"
+              href="/posts/wall-sd"
+              backgroundSrc="/images/posts/wall-sd/shelly-rabbit.JPG"
+              preview="Ride a jackalope and see a T-Rex in South Dakota..."
             />
           </div>
         </div>

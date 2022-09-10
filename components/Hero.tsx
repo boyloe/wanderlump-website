26f6cbdf0466aec5b58e419623401f1785f884fd
Bryan Oyloe <29239581+boyloe@users.noreply.github.com>
2022-09-10 14:32:17 -0600

    move subscribe cta above recent posts

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,13 +2,13 @@ import React, { ReactElement } from "react";
 
 export default function Hero(): ReactElement {
   return (
-    <div className="flex flex-col md:my-4 md:flex-row justify-around md:justify-center md:items-center font-josefin-sans md:h-screen md:relative">
+    <div className="flex flex-col md:mt-4 md:mb-72 md:flex-row justify-around md:justify-center md:items-center font-josefin-sans md:w-full md:max-h-screen md:relative">
       <img
-        src="images/lumpy_thoughtful_landscape.jpg"
+        src="images/lumpy_thoughtful_landscape_scaled.jpeg"
         alt="Lumpy looking out a window"
-        className="my-4"
+        className="p-4 md:p-0"
       />
-      <div className="md:w-auto flex flex-col items-center justify-center md:p-4 md:absolute left-6 top-4">
+      <div className="mt-4 md:w-auto flex flex-col items-center justify-center md:p-4 md:absolute left-8 top-24">
         <h1 className="text-dark-brown text-5xl md:text-8xl">wan·der·lump</h1>
         <h2 className="text-dark-brown font-medium text-center md:text-2xl text-2xl font-josefin-sans mx-2">
           Lumpy&apos;s irresistible desire to wander the world

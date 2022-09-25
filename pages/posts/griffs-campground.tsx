6380bd0c049485dbdff62e5be1c888cc6a3f2bbe
Bryan Oyloe <29239581+boyloe@users.noreply.github.com>
2022-09-25 17:01:59 -0600

    add comments to griffs

--- pages/posts/griffs-campground.tsx
+++ pages/posts/griffs-campground.tsx
@@ -1,96 +1,129 @@
 import React, { ReactElement } from "react";
+import Head from "next/head";
 import PrevNext from "../../components/PrevNext";
+import Comments from "../../components/Comments";
 
 export default function griffsCampground(): ReactElement {
   return (
-    <div className="mx-auto max-w-3xl p-4 md:p-0">
-      <title>Griff&apos;s RV Park and Campground</title>
-      <article>
-        <h1 className="text-3xl mb-4 font-lato font-black tracking-wide text-center">
-          Griff&apos;s Valley View RV Park &amp; Campground
-        </h1>
-        <img
-          src="/images/posts/griffs-campground/griffs-cropped.jpg"
-          alt="Bryan standing in front of the Griff's sign"
-          className="border-4 border-gray-900 shadow-2xl"
+    <>
+      <Head>
+        <title>Griff&apos;s RV Campground</title>
+        <meta property="og:title" content="Griff's RV Campground" />
+        <meta
+          property="og:description"
+          content="Griff's Campground, our first stop after buying our trailer"
         />
-        <p className="my-4 font-lato text-xl tracking-wide text-justify">
-          After picking up the trailer and getting an incredibly thorough walk
-          through from Brady at Herold Trailer Sales, we needed a place nearby
-          to unload everything from the truck into the trailer. We were also
-          exhausted from the previous few days of packing and driving to Iowa,
-          so we settled on a full hook up site just 30 minutes north in Altoona,
-          Iowa. I don&apos;t think we could have found a better site for our
-          first stay and to get unpacked! Our spot was right on a pond, the
-          sites were all concrete, perfectly level pads with lush green grass
-          all around. We were lucky to even get a spot, because most of the park
-          was full.
-        </p>
-        <h2 className="mt-10 mb-4 text-2xl font-lato font-black text-center tracking-wide">
-          Not bad for our first time...
-        </h2>
-        <p className="my-4 font-lato text-xl tracking-wide text-justify">
-          For it being Bryan’s first time backing up our new trailer, he was a
-          rock star. Perfect back up job on the first attempt. This was not
-          luck, though. He grew up towing boats and his previous career in the
-          oil industry required him to back up trailers frequently. Lumpy was
-          pretty impressed. Catalina was too busy sleeping in her carrier to be
-          bothered.{" "}
-        </p>
-        <p className="my-4 font-lato text-xl tracking-wide text-justify">
-          What came next was entertaining for anyone peeking out their window.
-          It was probably the world’s longest set up. In our defense, all of our
-          stuff was brand new and either still in the box or in the storage
-          bins, so it took a while to open everything up. We weren’t just
-          winging it; we had prepared and downloaded the incredibly handy
-          checklist, so we went step by step and after two hours, we felt fairly
-          confident we wouldn’t roll away into the pond or blow up.{" "}
-        </p>
-        <img
-          src="/images/posts/griffs-campground/truck-trailer.JPG"
-          alt="Bryan getting the trailer disconnected"
-          className="border-4 border-gray-900 shadow-2xl"
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://wanderlump.com/posts/griffs-campground"
         />
-        <h2 className="mt-10 mb-4 text-2xl font-lato font-black text-center">
-          Introductions
-        </h2>
-        <p className="my-4 font-lato text-xl tracking-wide text-justify">
-          Now that we were fully set up, it was time for the most crucial step –
-          introducing the cats to their new home. What if they hated it? What if
-          they were so stressed that they never came out of hiding? What if all
-          this planning was for nothing? Our fears quickly subsided as they took
-          time to sniff and explore every inch and then they both made
-          themselves at home on the bed.
-        </p>
-        <img
-          src="/images/posts/griffs-campground/lumpy-catalina.JPG"
-          alt="Lumpy and Catalina asleep on the bed"
-          className="border-4 border-gray-900 shadow-2xl"
+        <meta
+          property="og:image"
+          content="https://www.wanderlump.com/images/posts/griffs-campground/griffs-sign.JPG"
         />
-        <h2 className="mt-10 mb-4 text-2xl font-lato font-black text-center">
-          All work and no play?
-        </h2>
-        <p className="my-4 font-lato text-xl tracking-wide text-justify">
-          There was so much work to do – unpacking, organizing, making endless
-          shopping lists of additional supplies, and working our full time jobs.
-          However, there was a huge weight off our shoulders. We were actually
-          doing this! Now was the time to have some fun, too. We explored
-          Griff’s and the many statues scattered throughout the park. We checked
-          out the setup of the other rigs and took some notes. We explored
-          Altoona, tried some local beer, planned our next travels, and made our
-          travel trailer feel like home. Next stop – Round Lake Winery in Round
-          Lake, Minnesota.
-        </p>
-        <img
-          src="/images/posts/griffs-campground/shelly-cowboy.JPG"
-          alt="Shelly pretending to be a cowgirl"
-          className="border-4 border-gray-900 shadow-2xl mb-8"
+
+        <meta property="og:image:type" content="image/jpeg" />
+        <meta property="og:image:width" content="400" />
+        <meta property="og:image:height" content="300" />
+        <meta
+          property="og:image:alt"
+          content="Bryan standing by the Griff's entrance sign"
         />
-      </article>
-      <PrevNext
-        previousPostHref="/posts/our-new-home"
-        nextPostHref="/posts/round-lake"
-      />
-    </div>
+      </Head>
+      <div className="mx-auto max-w-3xl p-4 md:p-0">
+        <article>
+          <h1 className="text-3xl mb-4 font-lato font-black tracking-wide text-center">
+            Griff&apos;s Valley View RV Park &amp; Campground
+          </h1>
+          <img
+            src="/images/posts/griffs-campground/griffs-cropped.jpg"
+            alt="Bryan standing in front of the Griff's sign"
+            className="border-4 border-gray-900"
+          />
+          <p className="my-4 font-lato text-xl tracking-wide text-justify">
+            After picking up the trailer and getting an incredibly thorough walk
+            through from Brady at Herold Trailer Sales, we needed a place nearby
+            to unload everything from the truck into the trailer. We were also
+            exhausted from the previous few days of packing and driving to Iowa,
+            so we settled on a full hook up site just 30 minutes north in
+            Altoona, Iowa. I don&apos;t think we could have found a better site
+            for our first stay and to get unpacked! Our spot was right on a
+            pond, the sites were all concrete, perfectly level pads with lush
+            green grass all around. We were lucky to even get a spot, because
+            most of the park was full.
+          </p>
+          <h2 className="mt-10 mb-4 text-2xl font-lato font-black text-center tracking-wide">
+            Not bad for our first time...
+          </h2>
+          <p className="my-4 font-lato text-xl tracking-wide text-justify">
+            For it being Bryan’s first time backing up our new trailer, he was a
+            rock star. Perfect back up job on the first attempt. This was not
+            luck, though. He grew up towing boats and his previous career in the
+            oil industry required him to back up trailers frequently. Lumpy was
+            pretty impressed. Catalina was too busy sleeping in her carrier to
+            be bothered.{" "}
+          </p>
+          <p className="my-4 font-lato text-xl tracking-wide text-justify">
+            What came next was entertaining for anyone peeking out their window.
+            It was probably the world’s longest set up. In our defense, all of
+            our stuff was brand new and either still in the box or in the
+            storage bins, so it took a while to open everything up. We weren’t
+            just winging it; we had prepared and downloaded the incredibly handy
+            checklist, so we went step by step and after two hours, we felt
+            fairly confident we wouldn’t roll away into the pond or blow up.{" "}
+          </p>
+          <img
+            src="/images/posts/griffs-campground/truck-trailer.JPG"
+            alt="Bryan getting the trailer disconnected"
+            className="border-4 border-gray-900"
+          />
+          <h2 className="mt-10 mb-4 text-2xl font-lato font-black text-center">
+            Introductions
+          </h2>
+          <p className="my-4 font-lato text-xl tracking-wide text-justify">
+            Now that we were fully set up, it was time for the most crucial step
+            – introducing the cats to their new home. What if they hated it?
+            What if they were so stressed that they never came out of hiding?
+            What if all this planning was for nothing? Our fears quickly
+            subsided as they took time to sniff and explore every inch and then
+            they both made themselves at home on the bed.
+          </p>
+          <img
+            src="/images/posts/griffs-campground/lumpy-catalina.JPG"
+            alt="Lumpy and Catalina asleep on the bed"
+            className="border-4 border-gray-900"
+          />
+          <h2 className="mt-10 mb-4 text-2xl font-lato font-black text-center">
+            All work and no play?
+          </h2>
+          <p className="my-4 font-lato text-xl tracking-wide text-justify">
+            There was so much work to do – unpacking, organizing, making endless
+            shopping lists of additional supplies, and working our full time
+            jobs. However, there was a huge weight off our shoulders. We were
+            actually doing this! Now was the time to have some fun, too. We
+            explored Griff’s and the many statues scattered throughout the park.
+            We checked out the setup of the other rigs and took some notes. We
+            explored Altoona, tried some local beer, planned our next travels,
+            and made our travel trailer feel like home. Next stop – Round Lake
+            Winery in Round Lake, Minnesota.
+          </p>
+          <img
+            src="/images/posts/griffs-campground/shelly-cowboy.JPG"
+            alt="Shelly pretending to be a cowgirl"
+            className="border-4 border-gray-900 mb-8"
+          />
+        </article>
+        <PrevNext
+          previousPostHref="/posts/our-new-home"
+          nextPostHref="/posts/round-lake"
+        />
+        <Comments
+          url="posts/griffs-campground"
+          identifier="griffs-campground-comments"
+          title="Griff's RV Campground"
+        />
+      </div>
+    </>
   );
 }

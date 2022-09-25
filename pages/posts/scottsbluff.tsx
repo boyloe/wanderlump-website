5d94947c6ffed25942daa9018932a14fd85eecad
Bryan Oyloe <29239581+boyloe@users.noreply.github.com>
2022-09-25 17:23:33 -0600

    add comments to Scotts bluff

--- pages/posts/scottsbluff.tsx
+++ pages/posts/scottsbluff.tsx
@@ -1,200 +1,235 @@
 import React, { ReactElement } from "react";
+import Head from "next/head";
 import Image from "next/image";
 import PrevNext from "../../components/PrevNext";
+import Comments from "../../components/Comments";
 
 export default function scottsbluff(): ReactElement {
   return (
-    <div>
-      <div className="w-screen mb-4 px-4 md:px-8">
-        <div className="mx-auto max-w-3xl">
-          <h1 className="text-3xl my-4 font-lato font-black tracking-wide text-center">
-            Scotts Bluff National Monument
-          </h1>
-        </div>
-        <div className="border-4 border-gray-900">
-          <Image
-            src="/images/posts/scotts-bluff/scottsbluff-pano.jpg"
-            alt="Panoramic view of Scotts Bluff National Monument"
-            height={600}
-            width={1200}
-            layout="responsive"
-          />
-        </div>
-      </div>
-      <div className="mx-auto max-w-3xl p-4 md:p-0">
+    <>
+      <Head>
         <title>Scotts Bluff National Monument</title>
-        <article>
-          <h2 className="mt-2 mb-4 text-2xl font-lato font-black text-center tracking-wide">
-            You Have Died of Dysentery
-          </h2>
-          <p className="my-4 font-lato text-xl tracking-wide text-justify">
-            After our wonderful stay at Papa Moon, we decided to stay in
-            Scottsbluff for the week at Riverside Park Campground. Our site was
-            just down the street from the city’s namesake – Scotts Bluff
-            National Monument. We hiked several trails by the monument,
-            including part of the Oregon Trail. It was an easy one mile out and
-            back trail and it had an audio tour giving historic details as we
-            walked along the path. There are several wagons along the trail to
-            demonstrate the wagons used by families. Pulling one of the wagons
-            is an ox who has clearly seen some things and his eyes are forever
-            unclean. There aren’t any visible wheel ruts along the trail due to
-            150 years of erosion, but there is a wagon swale, which is a deep
-            indentation created by the wagons traveling along Mitchell Pass.
-          </p>
-          <div className="grid grid-cols-2">
-            <div className="border-t-4 border-l-4 border-r-2 border-b-4 border-gray-900">
-              <Image
-                src="/images/posts/scotts-bluff/sun-between.JPG"
-                alt="Sun setting between the bluffs"
-                height={500}
-                width={600}
-                layout="responsive"
-              />
-              <Image
-                src="/images/posts/scotts-bluff/crazy-oxen.JPG"
-                alt="scotts-bluff"
-                height={500}
-                width={600}
-                layout="responsive"
-              />
-            </div>
-            <div className="border-t-4 border-r-4 border-b-4 border-l-2 border-gray-900">
-              <Image
-                src="/images/posts/scotts-bluff/white-wagon.JPG"
-                alt="scotts-bluff"
-                height={1000}
-                width={600}
-                layout="responsive"
-              />
-            </div>
+        <meta property="og:title" content="Round Lake, MN" />
+        <meta
+          property="og:description"
+          content="Scotts Bluff National Monument"
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://wanderlump.com/posts/scottsbluff"
+        />
+        <meta
+          property="og:image"
+          content="https://www.wanderlump.com/images/posts/scottsbluff/white-wagon.JPG"
+        />
+
+        <meta property="og:image:type" content="image/jpeg" />
+        <meta property="og:image:width" content="400" />
+        <meta property="og:image:height" content="300" />
+        <meta
+          property="og:image:alt"
+          content="White pioneer wagon near Scotts Bluff National Monument"
+        />
+      </Head>
+      <div>
+        <div className="w-screen mb-4 px-4 md:px-8">
+          <div className="mx-auto max-w-3xl">
+            <h1 className="text-3xl my-4 font-lato font-black tracking-wide text-center">
+              Scotts Bluff National Monument
+            </h1>
           </div>
-          <h2 className="mt-10 mb-4 text-2xl font-lato font-black text-center tracking-wide">
-            Saddle Rock Trail
-          </h2>
-          <p className="my-4 font-lato text-xl tracking-wide text-justify">
-            This was a fun and educational hike, because it also had an audio
-            guided tour, and took us to the summit of Scotts Bluff. We learned
-            that Scotts Bluff is named after the fur trader, Hiram Scott, who
-            became too ill to travel and was left behind by fellow trappers. His
-            remains were later found sixty miles away from where they abandoned
-            him. We also learned that the bluff was an important landmark for
-            not only the travelers along the Oregon Trail, but also the
-            California, Pony Express, and Mormon Trails.
-          </p>
           <div className="border-4 border-gray-900">
             <Image
-              src="/images/posts/scotts-bluff/bluff-overview.JPG"
-              alt="View from halfway up Saddle Rock Trail"
-              height={800}
+              src="/images/posts/scotts-bluff/scottsbluff-pano.jpg"
+              alt="Panoramic view of Scotts Bluff National Monument"
+              height={384}
               width={1200}
               layout="responsive"
             />
           </div>
-          <p className="my-4 font-lato text-xl tracking-wide text-justify">
-            Saddle Rock Trail starts out easy with a level path along the
-            prairie and then we made our way up a steep path to a tunnel. This
-            100-foot tunnel was hand carved with mainly picks and shovels,
-            because the rock layers were too soft to use any machinery.
-          </p>
-          <div className="border-4 border-gray-900">
-            <Image
-              src="/images/posts/scotts-bluff/shelly-tunnel.JPG"
-              alt="View from halfway up Saddle Rock Trail"
-              height={600}
-              width={500}
-              layout="responsive"
-            />
-          </div>
-          <p className="my-4 font-lato text-xl tracking-wide text-justify">
-            On the other side of the tunnel we were rewarded with an amazing
-            view of the city of Scottsbluff.
-          </p>
-          <div className="border-4 border-gray-900">
-            <Image
-              src="/images/posts/scotts-bluff/town-view.JPG"
-              alt="View of Scottsbluff, NE from the Saddle Rock Trail"
-              height={400}
-              width={500}
-              layout="responsive"
-            />
-          </div>
-          <p className="my-4 font-lato text-xl tracking-wide text-justify">
-            Next comes the fun part – switchbacks for days, which took us all
-            the way up to Saddle Rock. The formation is so fragile you can’t go
-            near it, but it is still striking from a distance.
-          </p>
-          <div className="border-4 border-gray-900">
-            <Image
-              src="/images/posts/scotts-bluff/collapsed-bluff.JPG"
-              alt="Crumbling pieces of rock on Saddle Rock"
-              height={650}
-              width={500}
-              layout="responsive"
-            />
-          </div>
-          <p className="my-4 font-lato text-xl tracking-wide text-justify">
-            On the way to the summit, the path took us right along the edge of
-            the bluff, where the audio tour explains the layers of the bluff and
-            how the bluff is ever changing due to erosion. We could feel the
-            obvious difference in the layers, with the limestone being as hard
-            as concrete and the sandstone easily coming off on Bryan’s
-            fingertips.
-          </p>
-          <div className="border-4 border-gray-900">
-            <Image
-              src="/images/posts/scotts-bluff/sandstone.JPG"
-              alt="Sandstone and limestome on Saddle Rock"
-              height={650}
-              width={500}
-              layout="responsive"
-            />
-          </div>
-          <h2 className="mt-10 mb-4 text-2xl font-lato font-black text-center tracking-wide">
-            Great, but did the cats have fun in Nebraska?
-          </h2>
-          <p className="my-4 font-lato text-xl tracking-wide text-justify">
-            Yes. The weather was perfect, so we could leave the windows and
-            screen door open, allowing for what we like to call “screen time.”
-            They love sitting at the screen just daring a bug to come inside.
-            During the day, Lumpy and Catalina sunbathed on the back of the
-            chaise lounge, while Bryan and I worked long hours in order to pay
-            for their extravagant lifestyle.
-          </p>
-          <div className="grid grid-cols-4">
-            <div className="border-t-4 border-l-4 border-r-2 border-b-2 border-gray-900 col-span-2">
+        </div>
+        <div className="mx-auto max-w-3xl p-4 md:p-0">
+          <title>Scotts Bluff National Monument</title>
+          <article>
+            <h2 className="mt-2 mb-4 text-2xl font-lato font-black text-center tracking-wide">
+              You Have Died of Dysentery
+            </h2>
+            <p className="my-4 font-lato text-xl tracking-wide text-justify">
+              After our wonderful stay at Papa Moon, we decided to stay in
+              Scottsbluff for the week at Riverside Park Campground. Our site
+              was just down the street from the city’s namesake – Scotts Bluff
+              National Monument. We hiked several trails by the monument,
+              including part of the Oregon Trail. It was an easy one mile out
+              and back trail and it had an audio tour giving historic details as
+              we walked along the path. There are several wagons along the trail
+              to demonstrate the wagons used by families. Pulling one of the
+              wagons is an ox who has clearly seen some things and his eyes are
+              forever unclean. There aren’t any visible wheel ruts along the
+              trail due to 150 years of erosion, but there is a wagon swale,
+              which is a deep indentation created by the wagons traveling along
+              Mitchell Pass.
+            </p>
+            <div className="grid grid-cols-2">
+              <div className="border-t-4 border-l-4 border-r-2 border-b-4 border-gray-900">
+                <Image
+                  src="/images/posts/scotts-bluff/sun-between.JPG"
+                  alt="Sun setting between the bluffs"
+                  height={500}
+                  width={600}
+                  layout="responsive"
+                />
+                <Image
+                  src="/images/posts/scotts-bluff/crazy-oxen.JPG"
+                  alt="scotts-bluff"
+                  height={500}
+                  width={600}
+                  layout="responsive"
+                />
+              </div>
+              <div className="border-t-4 border-r-4 border-b-4 border-l-2 border-gray-900">
+                <Image
+                  src="/images/posts/scotts-bluff/white-wagon.JPG"
+                  alt="scotts-bluff"
+                  height={1000}
+                  width={600}
+                  layout="responsive"
+                />
+              </div>
+            </div>
+            <h2 className="mt-10 mb-4 text-2xl font-lato font-black text-center tracking-wide">
+              Saddle Rock Trail
+            </h2>
+            <p className="my-4 font-lato text-xl tracking-wide text-justify">
+              This was a fun and educational hike, because it also had an audio
+              guided tour, and took us to the summit of Scotts Bluff. We learned
+              that Scotts Bluff is named after the fur trader, Hiram Scott, who
+              became too ill to travel and was left behind by fellow trappers.
+              His remains were later found sixty miles away from where they
+              abandoned him. We also learned that the bluff was an important
+              landmark for not only the travelers along the Oregon Trail, but
+              also the California, Pony Express, and Mormon Trails.
+            </p>
+            <div className="border-4 border-gray-900">
               <Image
-                src="/images/posts/scotts-bluff/lumpy.JPG"
-                alt="Car dinosaur"
+                src="/images/posts/scotts-bluff/bluff-overview.JPG"
+                alt="View from halfway up Saddle Rock Trail"
+                height={800}
+                width={1200}
+                layout="responsive"
+              />
+            </div>
+            <p className="my-4 font-lato text-xl tracking-wide text-justify">
+              Saddle Rock Trail starts out easy with a level path along the
+              prairie and then we made our way up a steep path to a tunnel. This
+              100-foot tunnel was hand carved with mainly picks and shovels,
+              because the rock layers were too soft to use any machinery.
+            </p>
+            <div className="border-4 border-gray-900">
+              <Image
+                src="/images/posts/scotts-bluff/shelly-tunnel.JPG"
+                alt="View from halfway up Saddle Rock Trail"
                 height={600}
                 width={500}
                 layout="responsive"
               />
             </div>
-            <div className="border-t-4 border-r-4 border-b-2 border-l-2 border-gray-900 col-span-2">
+            <p className="my-4 font-lato text-xl tracking-wide text-justify">
+              On the other side of the tunnel we were rewarded with an amazing
+              view of the city of Scottsbluff.
+            </p>
+            <div className="border-4 border-gray-900">
               <Image
-                src="/images/posts/scotts-bluff/shelly-lumpy.JPG"
-                alt="metal salmon sculpture"
-                height={600}
+                src="/images/posts/scotts-bluff/town-view.JPG"
+                alt="View of Scottsbluff, NE from the Saddle Rock Trail"
+                height={400}
                 width={500}
                 layout="responsive"
               />
             </div>
-            <div className="border-t-2 border-l-4 border-r-4 border-b-4 border-gray-900 col-span-4 mb-12">
+            <p className="my-4 font-lato text-xl tracking-wide text-justify">
+              Next comes the fun part – switchbacks for days, which took us all
+              the way up to Saddle Rock. The formation is so fragile you can’t
+              go near it, but it is still striking from a distance.
+            </p>
+            <div className="border-4 border-gray-900">
               <Image
-                src="/images/posts/scotts-bluff/screen-time.jpg"
-                alt="rusted station wagon"
-                height={600}
+                src="/images/posts/scotts-bluff/collapsed-bluff.JPG"
+                alt="Crumbling pieces of rock on Saddle Rock"
+                height={650}
                 width={500}
                 layout="responsive"
               />
             </div>
-          </div>
-        </article>
+            <p className="my-4 font-lato text-xl tracking-wide text-justify">
+              On the way to the summit, the path took us right along the edge of
+              the bluff, where the audio tour explains the layers of the bluff
+              and how the bluff is ever changing due to erosion. We could feel
+              the obvious difference in the layers, with the limestone being as
+              hard as concrete and the sandstone easily coming off on Bryan’s
+              fingertips.
+            </p>
+            <div className="border-4 border-gray-900">
+              <Image
+                src="/images/posts/scotts-bluff/sandstone.JPG"
+                alt="Sandstone and limestome on Saddle Rock"
+                height={650}
+                width={500}
+                layout="responsive"
+              />
+            </div>
+            <h2 className="mt-10 mb-4 text-2xl font-lato font-black text-center tracking-wide">
+              Great, but did the cats have fun in Nebraska?
+            </h2>
+            <p className="my-4 font-lato text-xl tracking-wide text-justify">
+              Yes. The weather was perfect, so we could leave the windows and
+              screen door open, allowing for what we like to call “screen time.”
+              They love sitting at the screen just daring a bug to come inside.
+              During the day, Lumpy and Catalina sunbathed on the back of the
+              chaise lounge, while Bryan and I worked long hours in order to pay
+              for their extravagant lifestyle.
+            </p>
+            <div className="grid grid-cols-4">
+              <div className="border-t-4 border-l-4 border-r-2 border-b-2 border-gray-900 col-span-2">
+                <Image
+                  src="/images/posts/scotts-bluff/lumpy.JPG"
+                  alt="Car dinosaur"
+                  height={600}
+                  width={500}
+                  layout="responsive"
+                />
+              </div>
+              <div className="border-t-4 border-r-4 border-b-2 border-l-2 border-gray-900 col-span-2">
+                <Image
+                  src="/images/posts/scotts-bluff/shelly-lumpy.JPG"
+                  alt="metal salmon sculpture"
+                  height={600}
+                  width={500}
+                  layout="responsive"
+                />
+              </div>
+              <div className="border-t-2 border-l-4 border-r-4 border-b-4 border-gray-900 col-span-4 mb-12">
+                <Image
+                  src="/images/posts/scotts-bluff/screen-time.jpg"
+                  alt="rusted station wagon"
+                  height={600}
+                  width={500}
+                  layout="responsive"
+                />
+              </div>
+            </div>
+          </article>
+        </div>
+        <PrevNext
+          previousPostHref="/posts/papamoon"
+          nextPostHref="/posts/sand-dunes"
+        />
+        <Comments
+          url="posts/scottsbluff"
+          identifier="scottsbluff-comments"
+          title="Scotts Bluff National Monument"
+        />
       </div>
-      <PrevNext
-        previousPostHref="/posts/papamoon"
-        nextPostHref="/posts/sand-dunes"
-      />
-    </div>
+    </>
   );
 }

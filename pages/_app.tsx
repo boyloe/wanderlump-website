89ad84ef5e1d4dac261fd7686949980eb3636ed9
Bryan Oyloe <29239581+boyloe@users.noreply.github.com>
2022-12-02 15:04:54 -0700

    Remove gtag (#54)
    * delete analytics file and refs
    
    * remove ga from app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,25 +4,9 @@ import { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import Head from "next/head";
 import NavBar from "../components/NavBar";
-import * as ga from "../lib/ga/index";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      ga.pageview(url);
-    };
-    //When the component is mounted, subscribe to router changes
-    //and log those page views
-    router.events.on("routeChangeComplete", handleRouteChange);
-
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
 
   const canonicalUrl = (
     `https://www.wanderlump.com` + (router.asPath === "/" ? "" : router.asPath)
